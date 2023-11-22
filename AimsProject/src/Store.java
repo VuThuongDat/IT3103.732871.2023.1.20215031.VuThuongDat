be7601e60d157f1d92700f10406f276a901b031c
Datvu <vudatntnd@gmail.com>
2023-11-22 20:09:58 +0700

    store

---
+++ AimsProject/src/Store.java
@@ -0,0 +1,47 @@
+//Vu Thuong Dat 20215031
+public class Store {
+	private DigitalVideoDisc itemStore[];
+	private int qtyItem;
+
+	public Store(int MAX_NUMBER_ITEM) {
+		itemStore = new DigitalVideoDisc[MAX_NUMBER_ITEM];
+		qtyItem = 0;
+	}
+
+	public void addDVD(DigitalVideoDisc dvd) {
+		if (qtyItem < itemStore.length) {
+			itemStore[qtyItem] = dvd;
+			qtyItem++;
+			System.out.println("Them thanh cong " + dvd.getTitle());
+		} else {
+			System.out.println("Khong the them " + dvd.getTitle() +"vi cua hang da day!!");
+		}
+	}
+
+	public void removeDVD(DigitalVideoDisc dvd) {
+		boolean check = false; 
+		for (int i = 0; i < qtyItem; i++) {
+			if (itemStore[i].equals(dvd)) {
+				check = true;
+				for (int j = i; j < qtyItem - 1; j++) {
+					itemStore[j] = itemStore[j + 1];
+				}
+				qtyItem--;
+				System.out.println("Da xoa " + dvd.getTitle());
+				break;
+			}
+		}
+
+		if (!check) {
+			System.out.println("Khong tim thay " + dvd.getTitle());
+		}
+	}
+
+	public void displayStore() {
+		System.out.println("********** STORE **********");
+		for (int i = 0; i < qtyItem; i++) {
+			System.out.println((i + 1) + ". " + itemStore[i].toString());
+		}
+		System.out.println("***************************");
+	}
+}
---
+++ AimsProject/src/StoreTest.java
@@ -0,0 +1,42 @@
+//Vu Thuong Dat 20215031
+public class StoreTest {
+	public static void main(String[] args) {
+		Store store = new Store(10);
+		var dvd1 = new DigitalVideoDisc(
+				"The Lion King",
+				"Animation",
+				"Roger Allers",
+				87,
+				19.95f
+		);
+		var dvd2 = new DigitalVideoDisc(
+				"Star Wars",
+				"Science Fiction",
+				"George Lucas",
+				87,
+				24.95f
+		);
+		var dvd3 = new DigitalVideoDisc(
+				"Aladdin",
+				"Animation",
+				"Piaka",
+				92,
+				18.99f
+		);
+		var dvd4 = new DigitalVideoDisc(
+				"My Destiny",
+				"Love",
+				"Home Seckia",
+				97,
+				24.35f
+		);
+		
+		store.addDVD(dvd1);
+		store.addDVD(dvd2);
+		store.addDVD(dvd3);
+
+		store.displayStore();
+		store.removeDVD(dvd2);
+		store.displayStore();
+	}
+}
