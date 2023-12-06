2bada577d3963ee66c0b1c88197a409c2cea4a73
Datvu <vudatntnd@gmail.com>
2023-12-06 19:27:16 +0700

    add book class

--- AimsProject/src/hust/soict/dsai/aims/media/Book.java
+++ AimsProject/src/hust/soict/dsai/aims/media/Book.java
@@ -1,9 +1,28 @@
 package hust.soict.dsai.aims.media;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Book {
-
-	public Book() {
-		// TODO Auto-generated constructor stub
-	}
-
+	private int id;
+	private String title;
+	private String category;
+	private float cost;
+	private List<String> authors = new ArrayList<String>();
+	public void addAuthor(String authorname) {
+    	if(authors.contains(authorname)){
+    		System.out.println("Tac gia da co trong danh sach");
+    	}
+    	else {
+    		authors.add(authorname);
+    		System.out.println("Them thanh cong");
+    	}
+    }
+	 public void removeAuthor(String authorname) {
+	    	if(authors.contains(authorname)) {
+	    		authors.remove(authorname);
+	    		System.out.println("Xoa thanh cong");
+	    	}
+	    	else {
+	    		System.out.println("Tac gia khong co trong danh sach");
+	    	}
+	    }
 }
